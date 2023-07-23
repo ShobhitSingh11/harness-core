020d99521c1eb61040581b0292af3d58565068a5
Shobhit Singh <shobhit.singh@harness.io>
2023-07-23 19:49:09 +0530

    changes

--- 955-cg-yaml/src/main/java/software/wings/beans/InfraMappingYaml.java
+++ 955-cg-yaml/src/main/java/software/wings/beans/InfraMappingYaml.java
@@ -23,7 +23,6 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(callSuper = true)
 @OwnedBy(CDP)
 public abstract class InfraMappingYaml extends BaseEntityYaml {
- // private String serviceName;
   private String serviceName;
   private String infraMappingType;
   private String deploymentType;
