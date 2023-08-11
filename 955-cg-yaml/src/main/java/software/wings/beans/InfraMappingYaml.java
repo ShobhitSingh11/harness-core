6d7be8f20396c99511450b62a9358704b9513021
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2023-08-11 17:18:04 +0530

    Update InfraMappingYaml.java

--- 955-cg-yaml/src/main/java/software/wings/beans/InfraMappingYaml.java
+++ 955-cg-yaml/src/main/java/software/wings/beans/InfraMappingYaml.java
@@ -5,6 +5,7 @@
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
  */
 
+// package software.wings.beans;
 package software.wings.beans;
 
 import static io.harness.annotations.dev.HarnessTeam.CDP;
